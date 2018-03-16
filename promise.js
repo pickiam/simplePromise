547ebdf843c14465922bb8f5d2b803cced275ffd
web小狗 <1763242955@qq.com>
2018-03-15 22:14:56 -0700

    submit

--- promise.js
+++ promise.js
@@ -10,24 +10,33 @@ function Promise(resolver) {
         this.reject.call(this, error);
     }
 }
-
+//resolve作用主要是pending => resolved
 Promise.prototype.resolve = function(value) {
     if (!this.state == 'pending') return;
 
     this.state = 'fulfilled';
     this.value = value;
 
-}
+    setTimeout(() => {
+        if (!this.onfulfilled) return;
+        console.log(5)
+        this.onfulfilled();
+    }, 0);
 
+}
+//reject作用主要是pending => rejected
 Promise.prototype.reject = function(value) {
     if (!this.state == 'pending') return;
 
     this.state = 'rejected';
     this.value = value;
+    setTimeout(() => {
+        if (!this.onrejected) return;
+        this.onrejected();
+    }, 0);
 }
-
+//fulfilled,rejected的执行需要异步，then方法需要返回一个新的promise
 Promise.prototype.then = function(fulfilled, rejected) {
-
     if (typeof fulfilled !== 'function' && this.state === 'fulfilled' ||
         typeof rejected !== 'function' && this.state === 'rejected') {
         return this;
@@ -39,12 +48,22 @@ Promise.prototype.then = function(fulfilled, rejected) {
             var onfulfilled = function() {
                 try {
                     var result = fulfilled(self.value);
-                    resolve(result)
+                    if (result && result.then == 'function') {
+                        result.then(resolve,reject)
+                    } else {
+                        resolve(result)
+                    }
+                  
                 } catch (error) {
                     reject(error)
                 }
             }
-            onfulfilled()
+            if (that.state === 'pending') {
+                that.onfulfilled = onfulfilled;
+            } else {
+                onfulfilled()
+            }
+           
         }
 
         if (rejected && typeof rejected == 'function') {
@@ -60,3 +79,12 @@ Promise.prototype.then = function(fulfilled, rejected) {
         }
     })
 }
+
+var promise = new Promise((resolve,reject)=> {
+    console.log(2);
+    resolve('haha')
+})
+
+promise.then(res => {
+    return 3
+}).then(res => console.log(res))
